99a2b6fa16007f51cda82f74332bb98eea837e85
Facundo Abrahan Cerimeli <facundo.cerimeli@muttdata.ai>
2023-02-22 15:11:00 -0300

    add pytest

---
+++ src/src/example.py
@@ -0,0 +1,8 @@
+
+def sm(a: int, b: int) -> int:
+
+    """Test function"""
+
+    result: int = a + b
+
+    return result
---
+++ tests/unit/general/example_test.py
@@ -0,0 +1,8 @@
+from src.src.example import sm
+
+
+def test_sm() -> None:
+
+    """Test sm"""
+
+    assert sm(2, 2) == 4
