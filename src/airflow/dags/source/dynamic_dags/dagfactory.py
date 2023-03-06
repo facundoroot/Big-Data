63d88b8f1eba16951b488d9adef921ff737d7e8f
Facundo Abrahan Cerimeli <facundo.cerimeli@muttdata.ai>
2023-03-06 11:28:41 -0300

    returning to original structure before making it a package

--- src/airflow/dags/source/dynamic_dags/dagfactory.py
+++ src/airflow/dags/source/dynamic_dags/dagfactory.py
@@ -1,8 +1,7 @@
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-from datetime import datetime
 
-from src.airflow.dags.utils.helpers import logger
+import logging
+import colorlog
 
 from pathlib import Path
 
@@ -11,6 +10,40 @@ import utils
 from typing import List, Dict, Any
 
 import yaml
+
+
+def logger(name: str) -> logging.Logger:
+
+    """
+    Configure logger
+
+    Args:
+        *   name: Name for logger
+
+    Returns: Logger object with specified name and colors
+    """
+
+    formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(levelname)s%(reset)s %(name)s::%(asctime)s:: %(message)s",
+        log_colors={
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+        },
+        reset=True,
+        style='%',
+        datefmt='%d-%H:%M'
+    )
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    LOGGER = logging.getLogger(name)
+    LOGGER.addHandler(handler)
+    LOGGER.setLevel(logging.DEBUG)
+
+    return LOGGER
+
+
 
 LOGGER = logger('DagFactory')
 
@@ -44,11 +77,15 @@ class DagFactory:
             ) as file:
 
                 data = yaml.safe_load(file)
-                LOGGER.info(f"DAG YAML file path: {self.dag_yaml_path}")
+                LOGGER.info(
+                    f"DAG YAML file path: {self.dag_yaml_path}"
+                )
 
         except FileNotFoundError:
 
-            LOGGER.error(f"incorrect file path: {self.dag_yaml_path}")
+            LOGGER.error(
+                f"incorrect file path: {self.dag_yaml_path}"
+            )
             raise
 
         return data
@@ -65,8 +102,19 @@ class DagFactory:
         Returns: YAML content parsed into a config Dict
         """
 
-        return dict()
+        yaml_data = self._load_config_data()
 
+        dag_config: Dict[str, str] = yaml_data.get('dag_config', None)
+
+        if not dag_config:
+
+            LOGGER.error(f"no dag_config inside yaml file")
+            raise
+        else:
+
+            LOGGER.info(f"dag_config: {dag_config}")
+
+        return dag_config
 
 
     def create_dag(self) -> DAG:
@@ -91,7 +139,9 @@ def generate_dags() -> Dict[str, DAG]:
     """
 
     config_files: List[Path] = list(sorted(utils.DAG_DEFINITIONS_PATH.rglob("*.yml")))
-    LOGGER.info(f"{len(config_files)} DAG definitions found: {list(map(str, config_files))}")
+    LOGGER.info(
+        f"{len(config_files)} DAG definitions found: {list(map(str, config_files))}"
+    )
 
     generated_dags: Dict[str, DAG] = {}
 
