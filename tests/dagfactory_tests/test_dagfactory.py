73b65b898636cb915d0b337991fcb5009acfb573
Facundo Abrahan Cerimeli <facundo.cerimeli@muttdata.ai>
2023-04-03 12:54:14 -0300

    add test for dag factory helpers

---
+++ tests/dagfactory_tests/test_dagfactory.py
@@ -0,0 +1,9 @@
+import pytest
+
+from pathlib import Path
+from core.tools.dagfactory.helpers import load_yaml_content
+from core.tools.dagfactory.dag_factory import YAML_DEFINED_DAGS_PATH
+
+print(str(YAML_DEFINED_DAGS_PATH))
+
+
